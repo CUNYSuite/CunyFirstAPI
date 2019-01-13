fcd868603adcf3c11cba4c462015dd98ea873b0c
Ehud Adler <easports96@gmail.com>
2019-01-12 22:25:52 -0500

    Added pip dist

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cunyfirstapi",
+    version="0.0.1",
+    author="Ehud Adler",
+    author_email="ehud.adler62@qmail.cuny.edu",
+    description="A simple CUNYFIRST python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CUNYSuite/CunyFirstAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
