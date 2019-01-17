20bc37db206c1c40a81bf528a7aba75e6b5e9a22
Ehud Adler <Huddie@users.noreply.github.com>
2019-01-16 22:33:02 -0500

    Improved danger (#23)
    * Improved danger
    
    * Small text correction
    
    * Fixed import
    
    * Fixed true ~> True
    
    * Updated danger test grep
    
    * Updated
    
    * Dummy commit
    
    * Dummy commit
    
    * Added Ehud Adler to contributors list

---
+++ cunyfirstapi/tests/tests.py
@@ -0,0 +1,25 @@
+###***********************************###
+'''
+CUNYFirstAPI
+File: tests.py
+Author: Ehud Adler
+Core Maintainers: Ehud Adler, Akiva Sherman, 
+Yehuda Moskovits
+Copyright: Copyright 2019, CUNYSuite
+License: MIT
+'''
+###***********************************###
+import unittest
+
+class TestTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(True, True)
+
+def run_test():
+    unittest.main()
+
+def main():
+    run_test()
+
+if __name__ == '__main__':
+    main()
