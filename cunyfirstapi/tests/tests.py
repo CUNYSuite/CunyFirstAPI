8f566c61381ce456b22b69305aea073052cbe47f
ericshermancs <zensap92@gmail.com>
2019-01-22 23:10:39 +0200

    parseargs

--- cunyfirstapi/tests/tests.py
+++ cunyfirstapi/tests/tests.py
@@ -12,6 +12,12 @@ License: MIT
 import unittest
 from ..locations_enum import Location
 from ..cunyfirstapi import CUNYFirstAPI
+import argparse
+
+
+global username
+global password
+
 
 class TestTest(unittest.TestCase):
     def test(self):
@@ -19,19 +25,30 @@ class TestTest(unittest.TestCase):
 
 
 class ClassSearchTest(unittest.TestCase):
-    def test(self, username, password):
+    def test(self):
         api = CUNYFirstAPI(username, password)
         search_page = api.move_to(Location.class_search)
         results = search_page.submit_search(institution='QNS01', term='1192', \
             subject = 'CSCI', course_number='111')
         self.assertTrue(len(results['results']) > 0)
-        
+
 
 
 def run_test():
     unittest.main()
 
 def main():
+	global username
+	global password
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--username')
+	parser.add_argument('--password')
+
+	args = parser.parse_args
+	username = args.username
+	password = args.password
+
+
     run_test()
 
 if __name__ == '__main__':
