331a95ce2bc76a673e5fce676505c4487e86c8e0
ericshermancs <zensap92@gmail.com>
2019-01-23 14:49:48 +0200

    change tests to be like GN's

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -20,8 +20,8 @@ from cunyfirstapi import CUNYFirstAPI
 import argparse
 import sys
 
-global username
-global password
+username = ''
+password = ''
 
 
 class TestTest(unittest.TestCase):
@@ -31,6 +31,11 @@ class TestTest(unittest.TestCase):
 
 class ClassSearchTest(unittest.TestCase):
     def test(self):
+        if not is_ci():
+            self.assertTrue(True)
+
+        global username
+        global password
         api = CUNYFirstAPI(username, password)
         api.login()
         search_page = api.move_to(Locations.class_search)
@@ -40,24 +45,53 @@ class ClassSearchTest(unittest.TestCase):
 
 
 
-def run_test():
+def monkey_path_print():
     global username
     global password
+    global redacted_print_std
+    global redacted_print_err
+    ## Monkey Patching stdout to remove any sens. data
+    redacted_list = [username, password]
+    redacted_print_std = RedactedPrint(STDOutOptions.STDOUT, redacted_list)
+    redacted_print_err = RedactedPrint(STDOutOptions.ERROR, redacted_list)
+    redacted_print_std.enable()
+    redacted_print_err.enable()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--username')
-    parser.add_argument('--password')
-    parser.add_argument('unittest_args', nargs='*')
+def get_username_password():
+    global username
+    global password
+    with open(".env", "r") as f:
+        username = f.readline()
+        password = f.readline()
 
-    args = parser.parse_args()
 
-    username = args.username
-    password = args.password
+def is_local():
+    return not is_venus_mars() and not is_ci()
 
-    sys.argv[1:] = args.unittest_args
+def is_venus_mars():
+    return socket.gethostname() == "venus" or socket.gethostname() == "mars"
+
+def is_ci():
+    return os.path.isfile(".ci")
+
+
+def run_test():
+    scriptpath = script_path()
+    instancepath = instance_path()
+
+    if os.path.isfile(instancepath):
+        os.system('rm {0}'.format(instancepath))
+
+    os.system('touch {0}'.format(instancepath))
     unittest.main()
+    os.system('rm {0}'.format(instancepath))
 
 def main():
+    if is_ci():
+        print("Running on CI.....")
+        get_username_password()
+        monkey_path_print()
+        
     run_test()
 
 if __name__ == '__main__':
